0c59354b305f35e78cd3d1483146b0ffbd5c2e55
Ricardo Chagas <ricardo.chagas@peyess.com>
2022-08-04 15:22:43 -0300

    feat(sales-app): expose string resources through android application

--- sales-app/app/src/main/java/com/peyess/salesapp/app/SalesApplication.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/app/SalesApplication.kt
@@ -1,14 +1,29 @@
 package com.peyess.salesapp.app
 
 import android.app.Application
+import android.content.Context
+import android.content.res.Resources
 import com.airbnb.mvrx.Mavericks
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class SalesApplication: Application() {
+
+    companion object {
+        lateinit var context: Context
+        lateinit var string: (id: Int) -> String
+    }
+
     override fun onCreate() {
         super.onCreate()
+        context = this
+        string = {
+            context.resources.getString(it)
+        }
 
+        Firebase.initialize(this)
         Mavericks.initialize(this)
     }
 }

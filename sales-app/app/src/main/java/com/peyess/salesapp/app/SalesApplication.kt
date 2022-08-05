2761b0d67ca37edb611242be3e74002a67cb31c4
Ricardo Chagas <ricardo.chagas@peyess.com>
2022-08-05 10:30:06 -0300

    feat(sales-app): installs Timber for logging

--- sales-app/app/src/main/java/com/peyess/salesapp/app/SalesApplication.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/app/SalesApplication.kt
@@ -2,11 +2,11 @@ package com.peyess.salesapp.app
 
 import android.app.Application
 import android.content.Context
-import android.content.res.Resources
 import com.airbnb.mvrx.Mavericks
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.ktx.initialize
+import com.peyess.salesapp.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
 
 @HiltAndroidApp
 class SalesApplication: Application() {
@@ -18,12 +18,16 @@ class SalesApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         context = this
         string = {
             context.resources.getString(it)
         }
 
-        Firebase.initialize(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
         Mavericks.initialize(this)
     }
 }

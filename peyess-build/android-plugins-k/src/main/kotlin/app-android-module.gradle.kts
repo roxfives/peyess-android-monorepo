db9203ec4f7b94aa9cfcba5827181a845dc9be53
Ricardo Chagas <ricardo.chagas@peyess.com>
2022-08-24 09:37:47 -0300

    feat(sales-app): add release build

--- peyess-build/android-plugins-k/src/main/kotlin/app-android-module.gradle.kts
+++ peyess-build/android-plugins-k/src/main/kotlin/app-android-module.gradle.kts
@@ -1,19 +1,44 @@
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     id("android-module")
     id("com.android.application")
     id("kotlin-android")
 }
 
+//val keystorePropertiesFile = rootProject.file("keystore.properties")
+//val keystoreProperties = Properties()
+//keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
 android {
+//    signingConfigs {
+//        getByName("release") {
+//            keyAlias = keystoreProperties["keyAlias"] as String
+//            keyPassword = keystoreProperties["keyPassword"] as String
+//            storeFile = file(keystoreProperties["storeFile"] as String)
+//            storePassword = keystoreProperties["storePassword"] as String
+//        }
+//    }
+
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
+            isDebuggable = true
+
+            signingConfig = signingConfigs.getByName("debug")
+
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
 
         getByName("debug") {
             isDebuggable = true
             applicationIdSuffix = ".debug"
+
+//            signingConfig = signingConfigs.getByName("debug")
+
+            isDebuggable =  true
+            isMinifyEnabled =  false
         }
     }
 }

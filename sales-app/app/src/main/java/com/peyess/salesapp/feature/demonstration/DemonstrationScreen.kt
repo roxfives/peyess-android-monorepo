c81f90ab1ee2d35612997546fcece2a61a1a9499
Ricardo Chagas <ricardo.chagas@peyess.com>
2022-09-28 19:29:18 -0300

    feat(sales-app): add info content for the demo animations

--- sales-app/app/src/main/java/com/peyess/salesapp/feature/demonstration/DemonstrationScreen.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/feature/demonstration/DemonstrationScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.animation.core.snap
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -46,9 +45,16 @@ import com.peyess.salesapp.feature.demonstration.state.DemonstrationViewModel
 import com.peyess.salesapp.ui.component.chip.PeyessChipGroup
 import com.peyess.salesapp.ui.component.footer.PeyessNextStep
 import com.peyess.salesapp.ui.theme.SalesAppTheme
+import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.MaterialDialogState
+import com.vanpra.composematerialdialogs.rememberMaterialDialogState
+import com.vanpra.composematerialdialogs.title
 
 private val animationWidth = 420.dp
 private val animationHeight = 236.dp
+
+private val dialogSpacerHeight = 32.dp
+private val infoTextPadding = 32.dp
 
 private val animationDuration = 10000
 private val animationProgressStart = 0f
@@ -81,7 +87,6 @@ fun DemonstrationScreenImpl(
     onSelectShown: (shownName: String) -> Unit = {},
     onDone: () -> Unit = {},
 ) {
-
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -126,24 +131,32 @@ private fun MultiList(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_multi_category_0),
             animationId = R.raw.lottie_demonstration_multi_category_0,
+
+            infoContent = stringResource(id = R.string.demonstration_multi_category_0_info_content),
         )
 
         LensAnimation(
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_multi_category_1),
             animationId = R.raw.lottie_demonstration_multi_category_1,
+
+            infoContent = stringResource(id = R.string.demonstration_multi_category_1_info_content),
         )
 
         LensAnimation(
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_multi_category_2),
             animationId = R.raw.lottie_demonstration_multi_category_2,
+
+            infoContent = stringResource(id = R.string.demonstration_multi_category_2_info_content),
         )
 
         LensAnimation(
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_multi_category_3),
             animationId = R.raw.lottie_demonstration_multi_category_3,
+
+            infoContent = stringResource(id = R.string.demonstration_multi_category_3_info_content),
         )
     }
 }
@@ -159,24 +172,33 @@ private fun TreatmentsList(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_treatment_fat),
             animationId = R.raw.lottie_demonstration_treatment_fat,
+
+            infoContent = stringResource(id = R.string.demonstration_treatment_fat_info_content),
         )
 
         LensAnimation(
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_treatment_water),
             animationId = R.raw.lottie_demonstration_treatment_water,
+
+            infoContent = stringResource(id = R.string.demonstration_treatment_water_info_content),
         )
 
         LensAnimation(
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_treatment_damaged),
             animationId = R.raw.lottie_demonstration_treatment_damaged,
+
+            infoContent = stringResource(id = R.string.demonstration_treatment_damaged_info_content),
+            infoObs = stringResource(id = R.string.demonstration_treatment_damaged_info_obs),
         )
 
         LensAnimation(
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_treatment_clear),
             animationId = R.raw.lottie_demonstration_treatment_clear,
+
+            infoContent = stringResource(id = R.string.demonstration_treatment_clear_info_content),
         )
     }
 }
@@ -192,18 +214,24 @@ private fun PhotoList(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_transition_gray),
             animationId = R.raw.lottie_demonstration_transition_gray,
+
+            infoContent = stringResource(id = R.string.demonstration_transition_gray_info_content),
         )
 
         LensAnimation(
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_transition_brown),
             animationId = R.raw.lottie_demonstration_transition_brown,
+
+            infoContent = stringResource(id = R.string.demonstration_transition_brown_info_content),
         )
 
         LensAnimation(
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.demonstration_transition_green),
             animationId = R.raw.lottie_demonstration_transition_green,
+
+            infoContent = stringResource(id = R.string.demonstration_transition_green_info_content),
         )
     }
 }
@@ -217,8 +245,10 @@ private fun MiscList(modifier: Modifier = Modifier) {
     ) {
         LensAnimation(
             modifier = Modifier.fillMaxWidth(),
-            title = stringResource(id = R.string.demonstration_misc_simple),
-            animationId = R.raw.lottie_demonstration_misc_simple,
+            title = stringResource(id = R.string.demonstration_mono_simple),
+            animationId = R.raw.lottie_demonstration_mono_simple,
+
+            infoContent = stringResource(id = R.string.demonstration_mono_simple_info_content),
         )
     }
 }
@@ -226,8 +256,10 @@ private fun MiscList(modifier: Modifier = Modifier) {
 @Composable
 private fun LensAnimation(
     modifier: Modifier = Modifier,
-    title: String = "Alto Desempenho",
+    title: String = "",
     @RawRes animationId: Int = R.raw.lottie_demonstration_multi_category_0,
+    infoContent: String = "",
+    infoObs: String = "",
     onClickInfo: () -> Unit = {}
 ) {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(animationId))
@@ -250,6 +282,14 @@ private fun LensAnimation(
     ) {
         isPlaying.value = false
     }
+
+    val infoDialogState = rememberMaterialDialogState()
+    InfoDialog(
+        title = title,
+        dialogState = infoDialogState,
+        infoContent = infoContent,
+        infoObs = infoObs,
+    )
 
     Column(
         modifier = modifier,
@@ -281,7 +321,12 @@ private fun LensAnimation(
                 color = MaterialTheme.colors.primary.copy(alpha = 0.5f),
             )
 
-            IconButton(onClick = onClickInfo) {
+            IconButton(
+                onClick = {
+                    infoDialogState.show()
+                    onClickInfo()
+                },
+            ) {
                 Icon(imageVector = Icons.Filled.Info, contentDescription = "")
             }
         }
@@ -294,6 +339,48 @@ private fun LensAnimation(
             contentScale = ContentScale.FillWidth,
             progress = { progress }
         )
+    }
+}
+
+@Composable
+private fun InfoDialog(
+    modifier: Modifier = Modifier,
+
+    dialogState: MaterialDialogState = rememberMaterialDialogState(),
+
+    title: String = "",
+    infoContent: String = "",
+    infoObs: String = "",
+) {
+    // TODO: use string resource
+    MaterialDialog(
+        dialogState = dialogState,
+        buttons = { positiveButton("Legal!") },
+    ) {
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            title(title)
+
+            Text(
+                modifier = Modifier.padding(horizontal = infoTextPadding),
+                text = infoContent,
+                style = MaterialTheme.typography.body1
+                    .copy(textAlign = TextAlign.Center),
+            )
+
+            if (infoObs.isNotBlank()) {
+                Spacer(modifier = Modifier.height(dialogSpacerHeight))
+
+                Text(
+                    modifier = Modifier.padding(horizontal = infoTextPadding),
+                    text = infoObs,
+                    style = MaterialTheme.typography.caption,
+                )
+            }
+        }
     }
 }
 

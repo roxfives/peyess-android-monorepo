5919a0fb5547a15ced8fbf193fd28a9b97351b33
Ricardo Chagas <ricardo.chagas@peyess.com>
2022-10-18 08:27:30 -0300

    feat(sales-app): update scale for correction models

--- sales-app/app/src/main/java/com/peyess/salesapp/features/measuring_correction/internal/BridgeCorrectionModel.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/features/measuring_correction/internal/BridgeCorrectionModel.kt
@@ -4,15 +4,19 @@ import com.peyess.salesapp.data.model.raw_measuring.RawMeasuring
 import com.peyess.salesapp.features.measuring_correction.CorrectionModel
 import kotlin.math.round
 
+//['bridge', 'horizontal_check', 'vertical_check', 've', 'bottom_point', 'euler_angle_x']
+//[-0.29235279 -0.17628389 -0.1462711   0.12873821 -0.05510489  0.03842293]
+//8.55350834500817
+
 internal class BridgeCorrectionModel : CorrectionModel() {
     override fun fixField(rawMeasuring: RawMeasuring): Double {
-        val bridge = rawMeasuring.bridge * -0.63286615 //-0.69311305
-        val horizontalCheck = rawMeasuring.horizontalCheck * -0.25144899 //-0.2746577
-        val verticalCheck = rawMeasuring.verticalCheck * -0.07611089 //-0.0840033
-        val ve = rawMeasuring.ve * 0.19329001 //0.2447102
-        val bottomPoint = rawMeasuring.bottomPoint * -0.11161496 //-0.12043775
-        val eulerX = rawMeasuring.eulerAngleX * 0.13882194 //0.11192217
-        val intercept = 0.08683901736094168 //0.1367223274938127
+        val bridge = rawMeasuring.bridge * -0.29235279 //-0.63286615 //-0.69311305
+        val horizontalCheck = rawMeasuring.horizontalCheck * -0.17628389 //-0.25144899 //-0.2746577
+        val verticalCheck = rawMeasuring.verticalCheck * -0.1462711 //-0.07611089 //-0.0840033
+        val ve = rawMeasuring.ve * 0.12873821 //0.19329001 //0.2447102
+        val bottomPoint = rawMeasuring.bottomPoint * -0.05510489 //-0.11161496 //-0.12043775
+        val eulerX = rawMeasuring.eulerAngleX * 0.03842293 //0.13882194 //0.11192217
+        val intercept = 8.55350834500817 //0.08683901736094168 //0.1367223274938127
 
         val diff = bridge + horizontalCheck + verticalCheck + ve + bottomPoint + eulerX + intercept
         val prediction = rawMeasuring.bridge + diff

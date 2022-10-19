31240b247013313b44e50ee34fcf0de65b50ebb7
Ricardo Chagas <ricardo.chagas@peyess.com>
2022-10-19 15:14:03 -0300

    feat(sales-app): add listener to the service orders

--- sales-app/app/src/main/java/com/peyess/salesapp/data/dao/service_order/ServiceOrderDaoImpl.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/data/dao/service_order/ServiceOrderDaoImpl.kt
@@ -3,10 +3,13 @@ package com.peyess.salesapp.data.dao.service_order
 import com.google.firebase.firestore.Query
 import com.peyess.salesapp.R
 import com.peyess.salesapp.app.SalesApplication
+import com.peyess.salesapp.dao.client.firestore.FSClient
+import com.peyess.salesapp.dao.client.firestore.toDocument
 import com.peyess.salesapp.data.model.sale.service_order.FSServiceOrder
 import com.peyess.salesapp.firebase.FirebaseManager
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 import timber.log.Timber
 import javax.inject.Inject
@@ -15,37 +18,73 @@ class ServiceOrderDaoImpl @Inject constructor(
     val salesApplication: SalesApplication,
     val firebaseManager: FirebaseManager,
 ): ServiceOrderDao {
-    override fun serviceOrders(): Flow<List<FSServiceOrder>> = flow {
+    override fun serviceOrders(): Flow<List<FSServiceOrder>> = callbackFlow {
         val firestore = firebaseManager.storeFirestore
         if (firestore == null) {
-            return@flow
+            return@callbackFlow
         }
 
         val storeId = firebaseManager.currentStore!!.uid
-
         val soPath = salesApplication
             .stringResource(id = R.string.fs_col_so)
             .format(storeId)
 
-        val snaps = firestore
-            .collection(soPath)
+        // Filter by store access
+        firestore.collection(soPath)
             .orderBy(
                 salesApplication.stringResource(R.string.fs_field_so_created),
                 Query.Direction.DESCENDING,
             )
-            .get()
-            .addOnCompleteListener {
-                if (!it.isSuccessful) {
-                    Timber.e(it.exception)
+            .addSnapshotListener { snapshot, exception ->
+                if (exception != null) {
+                    Timber.e(exception, "Failed while fetching clients")
                 }
-            }
-            .await()
 
-        val serviceOrders = snaps.mapNotNull {
-                it.toObject(FSServiceOrder::class.java)
+                val serviceOrders = snapshot?.mapNotNull {
+                    try {
+                        it.toObject(FSServiceOrder::class.java)
+                    } catch (error: Throwable) {
+                        Timber.e(error, "Failed to convert client document")
+                        null
+                    }
+                }
+
+                Timber.i("Sending service orders $serviceOrders")
+                trySend(serviceOrders ?: emptyList())
             }
 
-        emit(serviceOrders)
+        awaitClose()
+
+//        val firestore = firebaseManager.storeFirestore
+//        if (firestore == null) {
+//            return@flow
+//        }
+//
+//        val storeId = firebaseManager.currentStore!!.uid
+//
+//        val soPath = salesApplication
+//            .stringResource(id = R.string.fs_col_so)
+//            .format(storeId)
+//
+//        val snaps = firestore
+//            .collection(soPath)
+//            .orderBy(
+//                salesApplication.stringResource(R.string.fs_field_so_created),
+//                Query.Direction.DESCENDING,
+//            )
+//            .get()
+//            .addOnCompleteListener {
+//                if (!it.isSuccessful) {
+//                    Timber.e(it.exception)
+//                }
+//            }
+//            .await()
+//
+//        val serviceOrders = snaps.mapNotNull {
+//                it.toObject(FSServiceOrder::class.java)
+//            }
+//
+//        emit(serviceOrders)
     }
 
     override suspend fun add(serviceOrder: FSServiceOrder) {

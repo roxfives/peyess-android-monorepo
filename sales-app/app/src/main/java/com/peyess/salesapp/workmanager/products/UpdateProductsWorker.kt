9b4a34b92151cd49333b461aad19639b1d8bb511
Ricardo Chagas <ricardo.chagas@peyess.com>
2022-12-20 17:23:41 -0300

    feat(saels-app): clear database before update and dismiss notification

--- sales-app/app/src/main/java/com/peyess/salesapp/workmanager/products/UpdateProductsWorker.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/workmanager/products/UpdateProductsWorker.kt
@@ -2,7 +2,6 @@ package com.peyess.salesapp.workmanager.products
 
 import android.app.PendingIntent
 import android.content.Context
-import android.database.sqlite.SQLiteConstraintException
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.hilt.work.HiltWorker
@@ -10,25 +9,11 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import arrow.core.getOrElse
 import arrow.core.left
+import arrow.core.right
 import arrow.fx.coroutines.Schedule
 import com.peyess.salesapp.BuildConfig
 import com.peyess.salesapp.R
 import com.peyess.salesapp.app.SalesApplication
-import com.peyess.salesapp.data.model.lens.FSStoreLocalLens
-import com.peyess.salesapp.data.model.lens.toFilterCategory
-import com.peyess.salesapp.data.model.lens.toFilterLensFamily
-import com.peyess.salesapp.data.model.lens.toFilterLensGroup
-import com.peyess.salesapp.data.model.lens.toFilterLensMaterial
-import com.peyess.salesapp.data.model.lens.toFilterLensSpecialty
-import com.peyess.salesapp.data.model.lens.toFilterLensSupplier
-import com.peyess.salesapp.data.model.lens.toFilterLensTech
-import com.peyess.salesapp.data.model.lens.toFilterLensType
-import com.peyess.salesapp.dao.products.room.join_lens_coloring.JoinLensColoringEntity
-import com.peyess.salesapp.dao.products.room.join_lens_material.JoinLensMaterialEntity
-import com.peyess.salesapp.dao.products.room.join_lens_tech.JoinLensTechEntity
-import com.peyess.salesapp.dao.products.room.join_lens_treatment.JoinLensTreatmentEntity
-import com.peyess.salesapp.dao.products.room.local_coloring.LocalColoringEntity
-import com.peyess.salesapp.dao.products.room.local_treatment.LocalTreatmentEntity
 import com.peyess.salesapp.data.adapter.lenses.colorings.extractColoring
 import com.peyess.salesapp.data.adapter.lenses.colorings.extractTreatment
 import com.peyess.salesapp.data.adapter.lenses.extractCategory
@@ -46,7 +31,6 @@ import com.peyess.salesapp.data.model.lens.StoreLensDocument
 import com.peyess.salesapp.data.model.lens.alt_height.StoreLensAltHeightDocument
 import com.peyess.salesapp.data.model.lens.coloring.StoreLensColoringDocument
 import com.peyess.salesapp.data.model.lens.treatment.StoreLensTreatmentDocument
-import com.peyess.salesapp.data.model.products_table_state.ProductsTableStatus
 import com.peyess.salesapp.data.repository.lenses.StoreLensResponse
 import com.peyess.salesapp.data.repository.lenses.StoreLensesRepository
 import com.peyess.salesapp.data.repository.lenses.room.LocalLensesRepository
@@ -57,7 +41,6 @@ import com.peyess.salesapp.data.utils.query.PeyessQueryOperation
 import com.peyess.salesapp.data.utils.query.buildQueryField
 import com.peyess.salesapp.data.utils.query.types.Order
 import com.peyess.salesapp.database.room.ProductsDatabase
-import com.peyess.salesapp.firebase.FirebaseManager
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Dispatchers
@@ -65,6 +48,8 @@ import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 
 const val forceUpdateKey = "UpdateProductsWorker_forceUpdate"
+
+const val notificationId = 0
 
 @HiltWorker
 class UpdateProductsWorker @AssistedInject constructor(
@@ -74,7 +59,6 @@ class UpdateProductsWorker @AssistedInject constructor(
     private val productsDatabase: ProductsDatabase,
     private val productsTableStateRepository: ProductsTableStateRepository,
     private val storeLensesRepository: StoreLensesRepository,
-    private val firebaseManager: FirebaseManager,
     private val localLensesRepository: LocalLensesRepository,
 ): CoroutineWorker(context, workerParams) {
 
@@ -228,8 +212,7 @@ class UpdateProductsWorker @AssistedInject constructor(
     }
 
     private fun clearAllProducts() {
-        Timber.i("Clearing all current local products")
-        // TODO: fix foreign key problem
+        Timber.i("clearAllProducts: Clearing all current local products")
         productsDatabase.clearAllTables()
     }
 
@@ -252,8 +235,7 @@ class UpdateProductsWorker @AssistedInject constructor(
             .setContentIntent(pendingIntent)
 
         with(NotificationManagerCompat.from(context)) {
-            // notificationId is a unique int for each notification that you must define
-            notify(0, builder.build())
+            notify(notificationId, builder.build())
         }
     }
 
@@ -261,7 +243,7 @@ class UpdateProductsWorker @AssistedInject constructor(
         val context = salesApplication as Context
 
         with(NotificationManagerCompat.from(context)) {
-            cancel(0)
+            cancel(notificationId)
         }
     }
 
@@ -281,31 +263,30 @@ class UpdateProductsWorker @AssistedInject constructor(
                 }
             }
 
-    override suspend fun doWork(): Result {
-        val firestore = firebaseManager.storeFirestore
-        if (firestore == null) {
-            Timber.e("Firestore instance is null")
-            productsTableStateRepository
-                .update(
-                    ProductsTableStatus(
-                        hasUpdated = false,
-                        hasUpdateFailed = true,
-                        isUpdating = false,
-                    )
+    private fun buildQuery(): PeyessQuery {
+        val queryFields = storeLensesRepository.queryFields
+
+        return PeyessQuery(
+            queryFields = listOf(
+                buildQueryField(
+                    field = queryFields.isLocalEnabled,
+                    op = PeyessQueryOperation.Equal,
+                    value = true,
                 )
+            ),
+            orderBy = PeyessOrderBy(
+                field = queryFields.priority,
+                order = Order.ASCENDING,
+            )
+        )
+    }
 
-            return Result.retry()
-        }
+    override suspend fun doWork(): Result {
+       Timber.i("doWork: Starting product update")
 
-        val storeId = firebaseManager.currentStore!!.uid
-        val lensPath = salesApplication
-            .stringResource(id = R.string.fs_col_lenses)
-            .format(storeId)
-
-        Timber.i("Starting product update")
         val result = runBlocking(Dispatchers.IO) {
             val forceUpdate = inputData.getBoolean(forceUpdateKey, false)
-            Timber.i("Starting Worker with forceUpdate = $forceUpdate")
+            Timber.i("doWork: Starting Worker with forceUpdate = $forceUpdate")
 
             val productsTableStatus = productsTableStateRepository.getCurrentState()
 
@@ -318,131 +299,42 @@ class UpdateProductsWorker @AssistedInject constructor(
                 return@runBlocking Result.success()
             }
 
-            productsTableStateRepository
-                .update(productsTableStatus.copy(isUpdating = true))
 
+            Timber.i("doWork: Cleaning up first...")
+            clearAllProducts()
+
+            Timber.i("doWork: Starting product update...")
+            productsTableStateRepository.update(
+                productsTableStatus.copy(isUpdating = true)
+            )
             createNotification()
 
-            Timber.i("Starting products update with path $lensPath")
-            val queryFields = storeLensesRepository.queryFields
+            Timber.i("doWork: Building query...")
+            val query = buildQuery()
 
-            val query = PeyessQuery(
-                queryFields = listOf(
-                    buildQueryField(
-                        field = queryFields.isLocalEnabled,
-                        op = PeyessQueryOperation.Equal,
-                        value = true,
-                    )
-                ),
-                orderBy = PeyessOrderBy(
-                    field = queryFields.priority,
-                    order = Order.ASCENDING,
-                )
-            )
-
+            Timber.i("doWork: Downloading and updating products...")
             val response = downloadAndPopulateProducts(query)
 
-            if (response.isLeft()) {
-                Timber.w("Failed to update products table: ${response.left()}")
-                return@runBlocking Result.retry()
+            Timber.i("doWork: Finished update with response $response")
+            return@runBlocking if (response.isLeft()) {
+                Timber.w("doWork: Failed to update products table: ${response.left()}")
+                Result.retry()
             } else {
-                return@runBlocking Result.success()
+                Timber.i("doWork: Update finished successfully: ${response.right()}")
+                productsTableStateRepository.update(
+                    productsTableStatus.copy(
+                        hasUpdated = true,
+                        hasUpdateFailed = false,
+                        isUpdating = false,
+                    )
+                )
+                dismissNotification()
+
+                Result.success()
             }
         }
 
         return result
-
-//        runBlocking(Dispatchers.IO) {
-//            val forceUpdate = inputData
-//                .getBoolean(forceUpdateKey, false)
-//            Timber.i("Starting Worker with forceUpdate = $forceUpdate")
-//
-//            val productsTableStatus = productsTableStateRepository.getCurrentState()
-//
-//            val isUpdating = productsTableStatus.isUpdating
-//            val needsUpdate = !productsTableStatus.hasUpdated
-//                    || productsTableStatus.hasUpdateFailed
-//
-//            val avoidUpdate = isUpdating || (!forceUpdate && !needsUpdate)
-//
-//            if (false) {
-//                return@runBlocking Result.success()
-//            }
-//
-//            productsTableStateRepository.update(
-//                productsTableStatus.copy(isUpdating = true),
-//            )
-//
-//            createNotification()
-//
-//            Timber.i("Starting products update with path $lensPath")
-//
-//            // TODO: use string resource
-//            // TODO: update enabled to is_local_enabled
-//            val initQuery = firestore
-//                .collection(lensPath)
-//                .whereEqualTo("is_local_enabled", true)
-//                .orderBy("priority")
-//                .limit(20)
-//                .get()
-//                .await()
-
-//            if (initQuery.size() != 1) {
-//                Timber.e("Initial query has to be of size 1, but it is ${initQuery.size()}")
-//                productsTableStateRepository
-//                    .update(
-//                        ProductsTableStatus(
-//                            hasUpdated = false,
-//                            hasUpdateFailed = true,
-//                            isUpdating = false,
-//                        )
-//                    )
-//
-//                return@runBlocking Result.retry()
-//            }
-
-//            clearAllProducts()
-//
-//            var snaps: QuerySnapshot
-//            var currSnapshot = initQuery.documents.last()
-//            var totalDownloaded = 0
-//            do {
-//                Timber.i("Starting page download")
-//                // TODO: use string resource
-//                // TODO: update enabled to is_local_enabled
-//                snaps = firestore
-//                    .collection(lensPath)
-//                    .whereEqualTo("is_local_enabled", true)
-//                    .orderBy("priority")
-//                    .startAfter(currSnapshot)
-//                    .limit(20)
-//                    .get()
-//                    .await()
-//                Timber.i("Downloaded total of ${snaps.size()}")
-//
-//                if (snaps.isEmpty) {
-//                    currSnapshot = null
-//                } else {
-//                    totalDownloaded += snaps.size()
-//                    currSnapshot = snaps.last()
-//                    addAllToLocalProducts(snaps.documents)
-//                }
-//            } while (!snaps.isEmpty)
-//
-//            Timber.i("Downloaded total of $totalDownloaded")
-//            productsTableStateRepository
-//                .update(
-//                    ProductsTableStatus(
-//                        hasUpdated = true,
-//                        hasUpdateFailed = false,
-//                        isUpdating = false,
-//                    )
-//                )
-//            dismissNotification()
-//        }
-//        Timber.i("Finished product update")
-//
-//        return Result.success()
     }
 
     companion object {

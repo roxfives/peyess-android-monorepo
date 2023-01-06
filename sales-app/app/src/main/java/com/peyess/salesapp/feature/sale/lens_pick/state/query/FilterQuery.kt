f010cfdcb7ad3744f3ac6564c412f37d3f47db5b
Ricardo Chagas <ricardo.chagas@peyess.com>
2023-01-06 17:23:03 -0300

    fix(sale-app): filter by supplier as well

--- sales-app/app/src/main/java/com/peyess/salesapp/feature/sale/lens_pick/state/query/FilterQuery.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/feature/sale/lens_pick/state/query/FilterQuery.kt
@@ -9,6 +9,14 @@ import com.peyess.salesapp.data.utils.query.types.Order
 import com.peyess.salesapp.feature.sale.lens_pick.constant.ListFilter
 import com.peyess.salesapp.feature.sale.lens_pick.model.LensListFilter
 
+private fun shouldFilterBySupplier(filter: ListFilter): Boolean {
+    return filter == ListFilter.LensFamily
+            || filter == ListFilter.LensDescription
+            || filter == ListFilter.LensMaterial
+            || filter == ListFilter.LensSpecialty
+            || filter == ListFilter.LensGroup
+}
+
 private fun shouldFilterByFamily(filter: ListFilter): Boolean {
     return filter == ListFilter.LensDescription
             || filter == ListFilter.LensMaterial
@@ -36,6 +44,16 @@ fun buildFilterQueryFields(
     activeListFilter: LensListFilter,
 ): List<PeyessQueryField> {
     val queryFields = mutableListOf<PeyessQueryField>()
+
+    if (shouldFilterBySupplier(filter) && activeListFilter.supplierId.isNotEmpty()) {
+        queryFields.add(
+            buildQueryField(
+                field = SimplifiedQueryFields.LensSupplier.name(),
+                op = PeyessQueryOperation.Equal,
+                value = activeListFilter.supplierId,
+            )
+        )
+    }
 
     if (shouldFilterByFamily(filter) && activeListFilter.familyId.isNotEmpty()) {
         queryFields.add(

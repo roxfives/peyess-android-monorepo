bc0d8803ce2a79eb23b2afc4ca2d24fbd7a71d0b
Ricardo Chagas <ricardo.chagas@peyess.com>
2023-01-30 14:06:08 -0300

    feat(sales-app): make return values nullable

--- sales-app/app/src/main/java/com/peyess/salesapp/data/dao/local_client/LocalClientDao.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/data/dao/local_client/LocalClientDao.kt
@@ -3,6 +3,7 @@ package com.peyess.salesapp.data.dao.local_client
 import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import androidx.room.RawQuery
 import androidx.room.Update
@@ -11,19 +12,17 @@ import com.peyess.salesapp.data.model.local_client.LocalClientEntity
 import com.peyess.salesapp.data.model.local_client.LocalClientStatusEntity
 import kotlinx.coroutines.flow.Flow
 
-private const val clientStatusId = 0L
-
 @Dao
 interface LocalClientDao {
-    @Insert
+    @Insert(onConflict = REPLACE)
     suspend fun insertClientStatus(clientStatus: LocalClientStatusEntity)
     @Update
     suspend fun updateClientStatus(clientStatus: LocalClientStatusEntity)
 
-    @Query("SELECT * FROM ${LocalClientStatusEntity.tableName} WHERE id = $clientStatusId")
-    suspend fun clientStatus(): LocalClientStatusEntity
-    @Query("SELECT * FROM ${LocalClientStatusEntity.tableName} WHERE id = $clientStatusId")
-    fun streamClientStatus(): Flow<LocalClientStatusEntity>
+    @Query("SELECT * FROM ${LocalClientStatusEntity.tableName} WHERE id = :id")
+    suspend fun clientStatus(id: Long): LocalClientStatusEntity?
+    @Query("SELECT * FROM ${LocalClientStatusEntity.tableName} WHERE id = :id")
+    fun streamClientStatus(id: Long): Flow<LocalClientStatusEntity?>
 
     @Insert
     suspend fun insertClient(clientStatus: LocalClientEntity)
@@ -31,9 +30,9 @@ interface LocalClientDao {
     suspend fun updateClient(clientStatus: LocalClientEntity)
 
     @Query("SELECT * FROM ${LocalClientEntity.tableName} WHERE id = :clientId")
-    suspend fun clientById(clientId: String): LocalClientEntity
+    suspend fun clientById(clientId: String): LocalClientEntity?
     @Query("SELECT * FROM ${LocalClientEntity.tableName} WHERE id = :clientId")
-    fun streamClientById(clientId: String): Flow<LocalClientEntity>
+    fun streamClientById(clientId: String): Flow<LocalClientEntity?>
 
     @RawQuery(observedEntities = [LocalClientEntity::class])
     fun paginateClients(query: SimpleSQLiteQuery): PagingSource<Int, LocalClientEntity>

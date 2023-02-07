3cc3e51d84e8e10e3de0700ab27caf506675e23f
Ricardo Chagas <ricardo.chagas@peyess.com>
2023-02-07 20:02:39 -0300

    style(sales-app): move variable annotations above them

--- sales-app/app/src/main/java/com/peyess/salesapp/dao/sale/product_picked/ProductPickedEntity.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/dao/sale/product_picked/ProductPickedEntity.kt
@@ -8,10 +8,16 @@ import androidx.room.PrimaryKey
     tableName = ProductPickedEntity.tableName,
 )
 data class ProductPickedEntity(
-    @PrimaryKey @ColumnInfo(name = "so_id") val soId: String = "",
-    @ColumnInfo(name = "lens_id") val lensId: String = "",
-    @ColumnInfo(name = "treatment_id") val treatmentId: String = "",
-    @ColumnInfo(name = "coloring_id") val coloringId: String = "",
+    @PrimaryKey
+    @ColumnInfo(name = "so_id")
+    val soId: String = "",
+
+    @ColumnInfo(name = "lens_id")
+    val lensId: String = "",
+    @ColumnInfo(name = "treatment_id")
+    val treatmentId: String = "",
+    @ColumnInfo(name = "coloring_id")
+    val coloringId: String = "",
 ) {
     companion object {
         const val tableName = "product_picked"

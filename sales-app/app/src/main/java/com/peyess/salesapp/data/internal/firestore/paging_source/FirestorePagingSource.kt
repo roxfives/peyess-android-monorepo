ec1b9f5a0b94c5b495f78a015e9c8f2dc80426e3
Ricardo Chagas <ricardo.chagas@peyess.com>
2023-02-07 15:28:45 -0300

    feat(sales-app): add snapshot listener to the firestore paging source

--- sales-app/app/src/main/java/com/peyess/salesapp/data/internal/firestore/paging_source/FirestorePagingSource.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/data/internal/firestore/paging_source/FirestorePagingSource.kt
@@ -2,9 +2,11 @@ package com.peyess.salesapp.data.internal.firestore.paging_source
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
-import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 import kotlin.reflect.KClass
 
 typealias PagingSourceResponse<F> = Pair<String, F>
@@ -12,7 +14,79 @@ typealias PagingSourceResponse<F> = Pair<String, F>
 abstract class FirestorePagingSource<F: Any> constructor(
     private val query: Query,
     private val type : KClass<out F>,
-): PagingSource<QuerySnapshot, PagingSourceResponse<F>>() {
+): PagingSource<DocumentSnapshot, PagingSourceResponse<F>>() {
+    override fun getRefreshKey(
+        state: PagingState<DocumentSnapshot, PagingSourceResponse<F>>,
+    ): DocumentSnapshot? {
+        return null
+    }
+
+    override suspend fun load(
+        params: LoadParams<DocumentSnapshot>,
+    ): LoadResult<DocumentSnapshot, PagingSourceResponse<F>> {
+
+
+        val nextPageKey = params.key
+        val pagingQuery = if (nextPageKey == null) {
+            initialQueryPage(query, params.loadSize.toLong())
+        } else {
+            subsequentQueryPage(query, params.loadSize.toLong(), nextPageKey)
+        }
+
+        return suspendCancellableCoroutine { continuation ->
+            pagingQuery.addSnapshotListener { snapshot, err ->
+                if (err != null) {
+                    if (continuation.isActive) {
+                        continuation.resume(
+                            LoadResult.Error(err)
+                        )
+                    } else {
+                        invalidate()
+                    }
+                } else if (snapshot != null && !snapshot.isEmpty) {
+                    val data = pageAdapter(snapshot)
+                    val nextKey = findNextPageKey(snapshot)
+
+                    if (continuation.isActive) {
+                        continuation.resume(
+                            LoadResult.Page(
+                                data = data,
+                                prevKey = null,
+                                nextKey = nextKey,
+                            )
+                        )
+                    } else {
+                        invalidate()
+                    }
+                } else {
+                    if (continuation.isActive) {
+                        continuation.resume(
+                            LoadResult.Page(
+                                data = emptyList(),
+                                prevKey = null,
+                                nextKey = null,
+                            )
+                        )
+                    } else {
+                        invalidate()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initialQueryPage(query: Query, loadSize: Long): Query {
+        return query.limit(loadSize)
+    }
+
+    private fun subsequentQueryPage(query: Query, loadSize: Long, last: DocumentSnapshot): Query {
+        return query.startAfter(last).limit(loadSize)
+    }
+
+    private fun findNextPageKey(querySnapshot: QuerySnapshot): DocumentSnapshot {
+        return querySnapshot.documents.last()
+    }
+
     private fun pageAdapter(snapshot: QuerySnapshot): List<PagingSourceResponse<F>> {
         var obj: F?
 
@@ -24,39 +98,6 @@ abstract class FirestorePagingSource<F: Any> constructor(
             } else {
                 null
             }
-        }
-    }
-
-    private suspend fun findNextPage(currentPage: QuerySnapshot): QuerySnapshot {
-        val index = (currentPage.size() - 1).coerceAtLeast(0)
-        val lastVisibleProduct = currentPage.documents[index]
-
-        return query.startAfter(lastVisibleProduct)
-            .get()
-            .await()
-    }
-
-    override fun getRefreshKey(
-        state: PagingState<QuerySnapshot, PagingSourceResponse<F>>,
-    ): QuerySnapshot? {
-        return null
-    }
-
-    override suspend fun load(
-        params: LoadParams<QuerySnapshot>,
-    ): LoadResult<QuerySnapshot, PagingSourceResponse<F>> {
-        return try {
-            val currentPage = params.key ?: query.get().await()
-            val nextPage = findNextPage(currentPage)
-            val data = pageAdapter(currentPage)
-
-            LoadResult.Page(
-                data = data,
-                prevKey = null,
-                nextKey = nextPage
-            )
-        } catch (throwable: Throwable) {
-            LoadResult.Error(throwable)
         }
     }
 }

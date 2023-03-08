25a75968ef199d1a15437e966ba3ffc8f247b460
Ricardo Chagas <ricardo.chagas@peyess.com>
2023-03-08 17:45:35 -0300

    feat(sales-app): update client legal data

--- sales-app/app/src/main/java/com/peyess/salesapp/data/dao/client_legal/ClientLegalDao.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/data/dao/client_legal/ClientLegalDao.kt
@@ -1,7 +1,16 @@
 package com.peyess.salesapp.data.dao.client_legal
 
+import arrow.core.Either
+import com.peyess.salesapp.data.dao.client_legal.error.UpdateClientLegalDaoError
 import com.peyess.salesapp.data.model.client_legal.FSClientLegal
+
+typealias UpdateClientLegalResponse = Either<UpdateClientLegalDaoError, Unit>
 
 interface ClientLegalDao {
     suspend fun addClientLegalFor(clientId: String, clientLegal: FSClientLegal)
+
+    suspend fun updateClientLegalFor(
+        clientId: String,
+        clientLegal: FSClientLegal,
+    ): UpdateClientLegalResponse
 }
--- sales-app/app/src/main/java/com/peyess/salesapp/data/dao/client_legal/ClientLegalDaoImpl.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/data/dao/client_legal/ClientLegalDaoImpl.kt
@@ -1,7 +1,13 @@
 package com.peyess.salesapp.data.dao.client_legal
 
+import arrow.core.Either
+import arrow.core.continuations.either
+import arrow.core.continuations.ensureNotNull
 import com.peyess.salesapp.R
 import com.peyess.salesapp.app.SalesApplication
+import com.peyess.salesapp.data.dao.client.error.UpdateClientDaoError
+import com.peyess.salesapp.data.dao.client_legal.adapter.toMap
+import com.peyess.salesapp.data.dao.client_legal.error.UpdateClientLegalDaoError
 import com.peyess.salesapp.data.model.client_legal.FSClientLegal
 import com.peyess.salesapp.firebase.FirebaseManager
 import kotlinx.coroutines.tasks.await
@@ -30,5 +36,32 @@ class ClientLegalDaoImpl @Inject constructor(
             Timber.e(err, "Error while adding client")
             error(err)
         }
+    }
+
+    override suspend fun updateClientLegalFor(
+        clientId: String,
+        clientLegal: FSClientLegal,
+    ): UpdateClientLegalResponse = either {
+        val firestore = firebaseManager.storeFirestore
+        ensureNotNull(firestore) {
+            UpdateClientLegalDaoError.UnexpectedError(
+                description = "Firestore instance is null",
+            )
+        }
+
+        Either.catch {
+            firestore
+                .document(
+                    salesApplication
+                        .stringResource(R.string.fs_doc_client_legal)
+                        .format(clientId)
+                ).update(clientLegal.toMap())
+                .await()
+        }.mapLeft {
+            UpdateClientLegalDaoError.UnexpectedError(
+                description = "Error while updating client",
+                throwable = it,
+            )
+        }.bind()
     }
 }

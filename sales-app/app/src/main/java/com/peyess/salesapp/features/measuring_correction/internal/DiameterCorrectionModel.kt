62d19604511d2888782800c7ca1409e33c7caf4e
Ricardo Chagas <ricardo.chagas@peyess.com>
2023-03-13 17:16:02 -0300

    refactor(sales-app): change algorithm to round the diameter

--- sales-app/app/src/main/java/com/peyess/salesapp/features/measuring_correction/internal/DiameterCorrectionModel.kt
+++ sales-app/app/src/main/java/com/peyess/salesapp/features/measuring_correction/internal/DiameterCorrectionModel.kt
@@ -3,11 +3,12 @@ package com.peyess.salesapp.features.measuring_correction.internal
 import com.peyess.salesapp.data.model.raw_measuring.RawMeasuring
 import com.peyess.salesapp.features.measuring_correction.CorrectionModel
 import kotlin.math.ceil
+import kotlin.math.roundToInt
 
 internal class DiameterCorrectionModel: CorrectionModel() {
     override fun fixField(rawMeasuring: RawMeasuring): Double {
-        val rounded = ceil(2.0 * rawMeasuring.diameter) / 2.0
-        val safety = 3
+        val rounded = (2.0 * rawMeasuring.diameter).roundToInt() / 2.0
+        val safety = 1
 
         return rounded + safety
     }

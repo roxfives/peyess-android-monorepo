013d3230dcdfec0b1b873fc1cce4a98a6fd4c30b
Ricardo Chagas <ricardo.chagas@peyess.com>
2023-07-21 15:24:08 -0300

    chore(sales-app): update the app version name and code

--- peyess-build/sales-app-plugins/src/main/kotlin/sales-app.gradle.kts
+++ peyess-build/sales-app-plugins/src/main/kotlin/sales-app.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.peyess.salesapp"
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "0.0.40"
     }
 }
